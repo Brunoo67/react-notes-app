4277280b738f19178635cde437164a5042b86b64
Brunoo67 <bruno.cavalcanti02@proton.me>
2024-10-14 15:42:47 -0300

    Add date-fns for date formatting functionality

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
 import { NoteCard } from "./components/note-card";
+import { CreateNewCard } from "./components/create-note-card";
 
 export function App() {
   
+  const note = {
+    date: new Date(),
+    content: "Hello"
+  }
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -18,22 +23,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-            </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-            </p>
-        </div>
+        <CreateNewCard/>
+        <NoteCard note = {note}/>
+        <NoteCard note = {note}/>
+        <NoteCard note = {note}/>
 
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-
-             
       </div>
 
     </div>
